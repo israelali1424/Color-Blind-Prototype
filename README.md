ea53b123c1d66bf9b64863bedb28d9b95ec2e54f
Israel Ali <israelali1424@gmail.com>
2020-03-03 18:16:08 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This protype was made with the help of  jscolor - JavaScript Color Picker
+View  http://jscolor.com for the full project
---
+++ project.js
@@ -0,0 +1,16 @@
+function update(jscolor) {
+    // 'jscolor' instance can be used as a string
+    document.getElementById('rect').style.backgroundColor = '#' + jscolor
+}
+
+/**
+ * jscolor - JavaScript Color Picker
+ *
+ * @link    http://jscolor.com
+ * @license For open source use: GPLv3
+ *          For commercial use: JSColor Commercial License
+ * @author  Jan Odvarko
+ * @version 2.0.5
+ *
+ * See usage examples at http://jscolor.com/examples/
+ */
